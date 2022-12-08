67d1d5b27b3e08fdc8a6c7a59e0a36244f6c6993
Kirill <kirillvlklimov@gmail.com>
2022-12-07 21:13:20 -0600

    added ability to connect to sql

--- src/SQLInterface/DBConnection.java
+++ src/SQLInterface/DBConnection.java
@@ -1,4 +1,38 @@
 package SQLInterface;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.Properties;
 
 public class DBConnection {
+    //jdbc:postgresql://hostname:port/databaseName
+
+    public void test() {
+        connect();
+    }
+    private static Connection connect() {
+        Properties props = new Properties();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader(); //for accessing files not visible when running for stream;
+        InputStream inputStream = loader.getResourceAsStream("InfoResources1.properties");
+
+        try {
+            props.load(inputStream);
+            StringBuilder bldr = new StringBuilder();
+
+            bldr.append("jdbc:postgresql://");
+            bldr.append(props.getProperty("host"));
+            bldr.append(":");
+            bldr.append(props.getProperty("port"));
+            bldr.append("/");
+            bldr.append(props.getProperty("dbname"));
+            bldr.append("?user=");
+            bldr.append(props.getProperty("username"));
+            bldr.append("&");
+            bldr.append((props.getProperty("password")));
+
+            System.out.println(bldr.toString());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import SQLInterface.DBConnection;
+
+public class Main {
+    public static void main(String[] args) {
+        DBConnection instance = new DBConnection();
+        instance.test();
+    }
+}

10698cb7cb11128db65db6df3863093418fe9f3b
Kirill <kirillvlklimov@gmail.com>
2022-12-14 17:48:21 -0600

    minor changes to delete web.xml file, resort to this file if web.xml is necessary w servlets

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,63 @@
 import SQLInterface.DBConnection;
 import io.javalin.Javalin;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("OOO");
         Javalin webApp = Javalin.create();
         webApp = webApp.start(8080);
+
         webApp.get("/ping", (ctx) -> {
             ctx.result("pong!");
+            ctx.result(ctx.body());
+            ctx.status(200);
         });
 
         webApp.post("/storeData", ctx -> {
-            ctx.formParam("objectName");
-            ctx.bodyAsClass(Object.class);
-            DataStore.storeObject( name, obj);
+            String name = ctx.formParam("objectName");
+            Object obj = ctx.bodyAsClass(Object.class);
+            DataStore.storeObject(name, obj);
+            ctx.status(201);
         });
+
+        webApp.post("/storeUser", ctx -> {
+
+            String name = ctx.formParam("objectName");
+            System.out.println(name);
+            //ctx.result(ctx.body());
+            //System.out.println(ctx.body().toString());
+            ctx.status(201);
+        });
+
 
         webApp.get("/retrieveData", ctx ->{
             String name = ctx.formParam("objectName");
-            Object obj = DataStore.getObject()
-        })
-    }
+            Object obj = DataStore.getObject(name);
+            ctx.json(obj);
+            ctx.status(200);
+        });
+
+        webApp.get("/retrieveDataByPathName/{name}", ctx -> {
+            String name = ctx.pathParam("name");
+            Object obj = DataStore.getObject(name);
+            ctx.json(obj);
+            ctx.status(200);
+        });
+
+        webApp.post("/testFormParams", ctx -> {
+            System.out.println(ctx.formParams("test"));
+
+            Map<String, List<String>> paramsMap = ctx.formParamMap();
+            Set<String> keys = paramsMap.keySet();
+
+            for (String key : keys) {
+                System.out.println(paramsMap.get(key));
+            }
+        });
+        }
 }

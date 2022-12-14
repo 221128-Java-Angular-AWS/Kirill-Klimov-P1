909964ce7837c1148befa350baefb5a2f13f12e5
Kirill <kirillvlklimov@gmail.com>
2022-12-14 15:20:16 -0600

    First commit in Javalin branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
 import SQLInterface.DBConnection;
-
+import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
-        DBConnection instance = new DBConnection();
-        instance.test();
+        Javalin webApp = Javalin.create();
+        webApp = webApp.start(8080);
+        webApp.get("/ping", (ctx) -> {
+            ctx.result("pong!");
+        });
+
+        webApp.post("/storeData", ctx -> {
+            ctx.formParam("objectName");
+            ctx.bodyAsClass(Object.class);
+            DataStore.storeObject( name, obj);
+        });
+
+        webApp.get("/retrieveData", ctx ->{
+            String name = ctx.formParam("objectName");
+            Object obj = DataStore.getObject()
+        })
     }
 }
